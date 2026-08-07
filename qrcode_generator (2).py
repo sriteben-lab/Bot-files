1536c9deaad23fd9439c82c826085fd654d03fb8
sriteben-lab <sriteben@gmail.com>
2026-08-07 11:17:52 +0100

    Add files via upload

---
+++ qrcode_generator (2).py
@@ -0,0 +1,26 @@
+import qrcode
+import tempfile
+
+
+def generate_qr(network, address, amount):
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(address)
+
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="black", back_color="white")
+
+    temp = tempfile.NamedTemporaryFile(
+        suffix=".png",
+        delete=False,
+    )
+
+    image.save(temp.name)
+
+    return temp.name
